4722dbddf3289d542339188c9bc435bc899b8a7a
duckandpup <newdagate@gmail.com>
2025-06-14 17:40:05 +0000

    Changed react link to local link

--- src/App.jsx
+++ src/App.jsx
@@ -14,7 +14,7 @@ function App() {
         <p>
           <a
             className="App-link"
-            href="https://reactjs.org"
+            href="https://github.com/duckandpup/reactcodespace.git"
             target="_blank"
             rel="noopener noreferrer"
           >
